5464f44954ddc4842e2b1111229bbd0bbf207845
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-04-18 10:18:44 +0500

    add calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,49 +1,27 @@
-import readlineSync from 'readline-sync';
-import getRandomInt from '../random.js';
-
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!\nWhat is the result of the expression?`);
-
-const congratulations = `Congratulations, ${userName}!`;
+const rules = 'What is the result of the expression?';
 const randomOperation = [' + ', ' - ', ' * '];
 
-const calculator = () => {
-  let i = 1;
-  while (i <= 3) {
-    const randomNumberFirst = getRandomInt();
-    const randomNumberSecond = getRandomInt();
-    const randomIndex = Math.floor(Math.random() * (3 - 0)) + 0;
-    const qestion = randomNumberFirst + randomOperation[randomIndex] + randomNumberSecond;
-    console.log(`Question: ${qestion}`);
-    const userAnsver = readlineSync.question('Your answer: ');
-    let result = 0;
+const getRound = () => {
+  const randomNumberFirst = Math.floor(Math.random() * (101 - 1)) + 1;
+  const randomNumberSecond = Math.floor(Math.random() * (101 - 1)) + 1;
+  const randomIndex = Math.floor(Math.random() * (3 - 0));
+  const question = randomNumberFirst + randomOperation[randomIndex] + randomNumberSecond;
 
-    switch (randomOperation[randomIndex]) {
-      case (' + '):
-        result = (randomNumberFirst + randomNumberSecond).toString();
-        break;
-      case (' - '):
-        result = (randomNumberFirst - randomNumberSecond).toString();
-        break;
-      case (' * '):
-        result = (randomNumberFirst * randomNumberSecond).toString();
-        break;
-      default:
-        return result.toString();
-    }
-
-    if (result === userAnsver) {
-      console.log('Correct!');
-    } else if (result !== userAnsver) {
-      console.log(`'${userAnsver}' is wrong answer ;(. Correct answer was '${result}'.\nLet's try again, ${userName}!`);
+  let correctAnsver = 0;
+  switch (randomOperation[randomIndex]) {
+    case (' + '):
+      correctAnsver = (randomNumberFirst + randomNumberSecond).toString();
       break;
-    }
-    i += 1;
-    if (i === 4) {
-      console.log(congratulations);
-    }
+    case (' - '):
+      correctAnsver = (randomNumberFirst - randomNumberSecond).toString();
+      break;
+    case (' * '):
+      correctAnsver = (randomNumberFirst * randomNumberSecond).toString();
+      break;
+    default:
+      return question.toString();
   }
+  return [question, correctAnsver];
 };
 
-calculator();
+export { rules, getRound };

2932c56520e3ee135259f4bab55b4d8863abe782
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-05-12 17:19:16 +0500

    rename function getGCD

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,7 +3,7 @@ import runEngine from '../index.js';
 
 const rules = 'Find the greatest common divisor of given numbers.';
 
-const gcd = (number1, number2) => {
+const getGCD = (number1, number2) => {
   let correctAnswer = 0;
   for (let i = 1; i <= number1 && i <= number2; i += 1) {
     if (number1 % i === 0 && number2 % i === 0) {
@@ -16,9 +16,9 @@ const gcd = (number1, number2) => {
 const getRound = () => {
   const number1 = getRandomNumber(1, 100);
   const number2 = getRandomNumber(1, 100);
-  const correctAnswer = gcd(number1, number2);
+  const answer = getGCD(number1, number2);
   const question = `${number1} ${number2}`;
-  return [question, correctAnswer];
+  return [question, answer];
 };
 
 const runGcd = () => runEngine(rules, getRound);

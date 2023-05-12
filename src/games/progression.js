c41f1eb5746b3c3e3e4ba1bde5eb9ae0229655c3
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-05-12 17:21:29 +0500

    update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,32 +3,24 @@ import runEngine from '../index.js';
 
 const rules = 'What number is missing in the progression?';
 
-const minCounter = 2;
-const maxCounter = 4;
-const maxLength = 19;
-
 const getProgression = (start, length, step) => {
   const result = [];
-  let i = start;
-  while (i <= length) {
+  for (let i = start; result.length < length; i += step) {
     result.push(i);
-    i += step;
   }
   return result;
 };
 
 const getRound = () => {
   const start = getRandomNumber(1, 100);
-  const length = (start + maxLength);
-  const step = getRandomNumber(minCounter, maxCounter);
-  const misingElement = '..';
-  const progression = getProgression(start, length, step);
-  const randomIndex = getRandomNumber(1, progression.length - 1);
-  const correctAnswer = progression[randomIndex].toString();
-  progression[randomIndex] = misingElement;
+  const progressionLength = 10;
+  const step = getRandomNumber(2, 4);
+  const progression = getProgression(start, progressionLength, step);
+  const hiddenIndex = getRandomNumber(1, progression.length - 1);
+  const answer = progression[hiddenIndex].toString();
+  progression[hiddenIndex] = '..';
   const question = progression.join(' ');
-
-  return [question, correctAnswer];
+  return [question, answer];
 };
 
 const runProgression = () => runEngine(rules, getRound);

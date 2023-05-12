5fbf7397568d96a56a78f0174d5504f92c73687d
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-05-12 17:10:49 +0500

    update calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,27 +3,28 @@ import runEngine from '../index.js';
 
 const rules = 'What is the result of the expression?';
 
-const calculate = (operators, randomIndex, number1, number2) => {
-  switch (operators[randomIndex]) {
-    case (' + '):
-      return (number1 + number2);
-    case (' - '):
-      return (number1 - number2);
-    case (' * '):
-      return (number1 * number2);
+const calculate = (operator, number1, number2) => {
+  switch (operator) {
+    case '+':
+      return number1 + number2;
+    case '-':
+      return number1 - number2;
+    case '*':
+      return number1 * number2;
     default:
-      throw new Error(`Unknown operators: '${operators}'!`);
+      throw new Error(`Unknown operators: '${operator}'!`);
   }
 };
 
 const getRound = () => {
   const number1 = getRandomNumber(1, 100);
   const number2 = getRandomNumber(1, 100);
-  const randomIndex = getRandomNumber(0, 3);
-  const operators = [' + ', ' - ', ' * '];
-  const correctAnswer = calculate(operators, randomIndex, number1, number2).toString();
-  const question = number1 + operators[randomIndex] + number2;
-  return [question, correctAnswer];
+  const operators = ['+', '-', '*'];
+  const randomIndex = getRandomNumber(0, operators.length - 1);
+  const operator = operators[randomIndex];
+  const answer = calculate(operator, number1, number2).toString();
+  const question = `${number1} ${operator} ${number2}`;
+  return [question, answer];
 };
 
 const runCalc = () => runEngine(rules, getRound);
